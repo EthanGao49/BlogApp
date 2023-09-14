e63b2d1c2e943ac91cf38f83e02278024af2ea14
EthanGao49 <ethan.gao47@gmail.com>
2023-09-13 23:45:09 -0400

    create project through start.spring.io

---
+++ src/test/java/net/blog/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.blog.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
