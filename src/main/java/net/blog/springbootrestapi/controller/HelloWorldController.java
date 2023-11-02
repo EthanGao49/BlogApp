6597aa50516ec7da540b0d64e616c6ced2c55425
EthanGao49 <ethan.gao47@gmail.com>
2023-11-02 18:51:00 -0400

    configure the database

---
+++ src/main/java/net/blog/springbootrestapi/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package net.blog.springbootrestapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // Make class as a Spring MVC Controller
+@ResponseBody // Convert the return from Java object to json
+// = @RestController
+public class HelloWorldController {
+
+}
