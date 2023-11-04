7b0e30c28713b5553d9f5a7eedf7d5d79d470bfd
EthanGao49 <ethan.gao47@gmail.com>
2023-11-04 00:59:49 -0400

    support SpringDoc

--- src/main/java/net/blog/springbootrestapi/SpringbootRestApiApplication.java
+++ src/main/java/net/blog/springbootrestapi/SpringbootRestApiApplication.java
@@ -1,9 +1,14 @@
 package net.blog.springbootrestapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(title = "Spring Boot Blog Application with REST APIs")
+)
 public class SpringbootRestApiApplication {
 
 	public static void main(String[] args) {
