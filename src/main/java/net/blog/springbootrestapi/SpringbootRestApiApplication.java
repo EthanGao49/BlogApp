98feaa4ebe54e3db779164ea89b0c2273d610ee3
EthanGao49 <ethan.gao47@gmail.com>
2023-11-12 01:56:45 -0500

    support prod profile to deploy on AWS

--- src/main/java/net/blog/springbootrestapi/SpringbootRestApiApplication.java
+++ src/main/java/net/blog/springbootrestapi/SpringbootRestApiApplication.java
@@ -2,6 +2,10 @@ package net.blog.springbootrestapi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import net.blog.springbootrestapi.entity.Role;
+import net.blog.springbootrestapi.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +13,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition(
 	info = @Info(title = "Spring Boot Blog Application with REST APIs")
 )
-public class SpringbootRestApiApplication {
+public class SpringbootRestApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestApiApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("Role_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
